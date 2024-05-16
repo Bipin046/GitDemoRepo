1942e9d2586fedd0fc5862e53fc82da94f423249
Bipin046 <163429855+Bipin046@users.noreply.github.com>
2024-05-16 13:56:44 +0530

    Update test2.spec.ts

--- test2.spec.ts
+++ test2.spec.ts
@@ -2,9 +2,16 @@ console.log("test2")
 
 const num1 = 5;
 const num2 = 3;
+const num3 = 7;
 
 // add two numbers
 const sum = num1 + num2;
 
+// add three numbers
+const sumThreeNum= num1 + num2 + num3
+
 // display the sum
 console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+
+// display the sum of three number
+console.log('The sum of three number is: ' + sumThreeNum)

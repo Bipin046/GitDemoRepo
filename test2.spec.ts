d8a6658ccf8c4867ca4844b4b8db40fcc4324f2c
Bipin Sharma <bipin@heady.io>
2024-05-16 15:13:04 +0530

    test pull

--- test2.spec.ts
+++ test2.spec.ts
@@ -3,10 +3,12 @@ console.log("test2")
 const num1 = 5;
 const num2 = 3;
 const num3= 7;
+const num4= 8;
 
 
 // add two numbers
 const sum = num1 + num2 + num3;
+const sumTwoNum=num1+num4;
 
 // display the sum
 console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);

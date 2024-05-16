65bb8259aa4bc93fd5ff36fbc63b9434b58b40fb
Bipin Sharma <bipin@heady.io>
2024-05-16 14:51:11 +0530

    remove the old code

--- test2.spec.ts
+++ test2.spec.ts
@@ -1,29 +1,10 @@
 console.log("test2")
-console.log("test3")
 
 const num1 = 5;
 const num2 = 3;
-const num3 = 7;
 
 // add two numbers
 const sum = num1 + num2;
 
-// add three numbers
-const sumThreeNum= num1 + num2 + num3
-
 // display the sum
 console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
-
-
-const num4= 6;
-const num5= 8;
-
-//substract
-const substract = num5 - num4;
-console.log('substarction result', substract)
-
-//multiply
-const mul = num1 * num2
-
-//divide
-const divide = num5/num4;

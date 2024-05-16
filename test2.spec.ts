b6d29a67fbf78a45057c66deba63f6e26de93780
Bipin046 <163429855+Bipin046@users.noreply.github.com>
2024-05-16 15:09:04 +0530

    Update test2.spec.ts

--- test2.spec.ts
+++ test2.spec.ts
@@ -2,9 +2,11 @@ console.log("test2")
 
 const num1 = 5;
 const num2 = 3;
+const num3= 7;
+
 
 // add two numbers
-const sum = num1 + num2;
+const sum = num1 + num2 + num3;
 
 // display the sum
 console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
